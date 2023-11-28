716bd7ba474fdf1a18922a0a1e7bcec589da03bf
Giulia Real <giuliaguerreiro9@gmail.com>
2023-11-27 21:01:40 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dscatalog.entities.Role;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
